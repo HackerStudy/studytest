7f44af959045b9acc88fba368e9b5a74d74b7a46
HackerStudy <1791133899@qq.com>
2019-03-29 16:31:18 +0800

    显示所有的操作系统环境变量

---
+++ src/main/java/com/hackerstudy/studytest/environment/EnvironmentVariables.java
@@ -0,0 +1,29 @@
+package com.hackerstudy.studytest.environment;
+
+import sun.awt.SunHints;
+
+import java.util.Map;
+
+/**
+ * @class: EnvironmentVariables
+ * @description: 显示所有的操作系统环境变量
+ * @author: yangpeng03614
+ * @date: 2019-03-29 15:27
+ */
+public class EnvironmentVariables {
+
+    /**
+     * main函数
+     * @param args
+     */
+    public static void main(String[] args) {
+        for(Map.Entry entry:System.getenv().entrySet()){
+            System.out.println(entry.getKey()+": "+entry.getValue());
+        }
+
+        //lambda表达式写佛Map的forEach
+        System.getenv().forEach((key,value) -> {
+            System.out.println(key+": "+value);
+        });
+    }
+}
