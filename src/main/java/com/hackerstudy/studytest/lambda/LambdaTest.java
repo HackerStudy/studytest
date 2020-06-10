f4104b9320a985a51274d9307bc13d6aa7db2dc2
HackerStudy <1791133899@qq.com>
2020-06-10 18:53:12 +0800

    lambda表达式的语法补充

--- src/main/java/com/hackerstudy/studytest/lambda/LambdaTest.java
+++ src/main/java/com/hackerstudy/studytest/lambda/LambdaTest.java
@@ -1,5 +1,8 @@
 package com.hackerstudy.studytest.lambda;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @class: LambdaTest
  * @description: lambda表达式的测试
@@ -29,17 +32,57 @@ public class LambdaTest {
     }
 
     /**
-     * 用lambda表达式实现
+     * 用lambda表达式实现多行
      */
     public static void multilineLambda(){
-        IMessage iMessage = (str) -> {
-            System.out.println(str);
-        };
+        IMessage iMessage = str -> System.out.println(str);
         iMessage.send("lambda");
     }
 
+    /**
+     * 用lambda表达式实现多行
+     */
     public static int onelineLambda(){
         IMath iMath =  (x,y) -> x+y;
         return iMath.add(1,2);
+    }
+
+    /**
+     * 带参数的lambda表达式
+     */
+    public static void parameterLambda(){
+        IMath iMath = (int x,int y) -> {
+            System.out.println("输出计算结果");
+            return x+y;
+        };
+        System.out.println(iMath.add(2,3));
+    }
+
+    /**
+     * 不带参数的lambda表达式
+     */
+    public static void noParameterLambda(){
+        NoParamter noParamter = () -> System.out.println("无参数输出");
+        noParamter.print();
+    }
+
+    /**
+     * forEach循环的lambda表达式
+     */
+    public static void forEachLambda(){
+        String[] strings = {"1","2","3"};
+        List<String> stringList= Arrays.asList(strings);
+        stringList.forEach((str) -> {
+            System.out.println(str);
+        });
+    }
+
+    /**
+     * Comparator的lambda表达式
+     */
+    public static void comparatorLambda(){
+        String[] strings = {"1","2","3"};
+        Arrays.sort(strings,(String str1,String str2) -> str1.compareTo(str2));
+        System.out.println(strings.toString());
     }
 }
---
+++ src/main/java/com/hackerstudy/studytest/lambda/NoParamter.java
@@ -0,0 +1,12 @@
+package com.hackerstudy.studytest.lambda;
+
+/**
+ * @class: NoParamter
+ * @description:
+ * @author: HackerStudy
+ * @date: 2020-06-10 18:21
+ */
+@FunctionalInterface
+public interface NoParamter {
+    void print();
+}
